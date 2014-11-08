c2aaf36b2a92c9ac97dacf47d992960916efefad
andy ccs <andychong2@gmail.com>
2014-11-08 17:01:21 +0800

    test precondition

--- CallCenter/createIncident.php
+++ CallCenter/createIncident.php
@@ -35,12 +35,13 @@ if($method == 'POST') {
 else if($method == 'GET') {
 ?>
 
-<html>
+<html ng-app>
 	<head>
 
 		<!-- Bootstrap -->
-		<link href="/css/bootstrap.min.css" rel="stylesheet">
-		
+		<link href="http://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet">
+		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.2/angular.min.js"></script>
+		<title>Create Incident</title>
 	</head>
 	<body>
 		
@@ -166,8 +167,6 @@ else if($method == 'GET') {
 		
 		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-	    <!-- Include all compiled plugins (below), or include individual files as needed -->
-	    <script src="js/bootstrap.min.js"></script>
 	</body>	
 </html>
 
--- test/spec.js
+++ test/spec.js
@@ -1,33 +1,48 @@
-describe('angularjs homepage', function() {
-  var firstNumber = element(by.model('first'));
-  var secondNumber = element(by.model('second'));
-  var goButton = element(by.id('gobutton'));
-  var latestResult = element(by.binding('latest'));
+describe('create incident function', function() {
+	var reporterNameUI = element(by.id('txtReporterName'));
+	var nricUI = element(by.id('txtNRIC'));
+	var mobileNoUI = element(by.id('txtMobileNo'));
+	var addressUI = element(by.id('txtAddress'));
+	var typeUI = element(by.id('cboTypeOfAssistance'));
+	var incidentNameUI = element(by.id('txtIncidentName'));
+	var incidentDescriptionUI = element(by.id('txtIncidentDescription'));
+	var priorityUI = element(by.id('cboPriority'));
 
-  beforeEach(function() {
-    browser.get('http://juliemr.github.io/protractor-demo/');
-  });
+	beforeEach(function() {
+		browser.get('http://localhost/CallCenter/createincident.php');
+	});
 
-  it('should have a title', function() {
-    expect(browser.getTitle()).toEqual('Super Calculator');
-  });
+	it('should have a correct title', function() {
+		expect(browser.getTitle()).toEqual('Create Incident');
+	});
 
-  it('should add one and two', function() {
-    firstNumber.sendKeys(1);
-    secondNumber.sendKeys(2);
+	it('should have fulfill all precondition', function() {
+		expect(reporterNameUI).not.toEqual(null);
+		expect(nricUI).not.toEqual(null);
+		expect(mobileNoUI).not.toEqual(null);
+		expect(addressUI).not.toEqual(null);
+		expect(typeUI).not.toEqual(null);
+		expect(incidentNameUI).not.toEqual(null);
+		expect(incidentDescriptionUI).not.toEqual(null);
+		expect(priorityUI).not.toEqual(null);
+	});
 
-    goButton.click();
+	// it('should add one and two', function() {
+	// 	firstNumber.sendKeys(1);
+	// 	secondNumber.sendKeys(2);
 
-    expect(latestResult.getText()).toEqual('3');
-  });
+	// 	goButton.click();
 
-  it('should add four and six', function() {
-    // Fill this in.
-    firstNumber.sendKeys(4);
-    secondNumber.sendKeys(6);
+	// 	expect(latestResult.getText()).toEqual('3');
+	// });
 
-    goButton.click();
-    
-    expect(latestResult.getText()).toEqual('10');
-  });
+	// it('should add four and six', function() {
+	// 	// Fill this in.
+	// 	firstNumber.sendKeys(4);
+	// 	secondNumber.sendKeys(6);
+
+	// 	goButton.click();
+
+	// 	expect(latestResult.getText()).toEqual('10');
+	// });
 });
