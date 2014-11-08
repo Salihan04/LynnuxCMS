5828751f5b8fdd8b143f51a5e0c28220f5f2c8ca
andy ccs <andychong2@gmail.com>
2014-11-08 10:57:52 +0800

    side navigation refine

--- operator/menu/operator_side_menu.php
+++ operator/menu/operator_side_menu.php
@@ -1,20 +1,18 @@
+<?php
+    function curPageURL($value) {
+        return preg_match("/^".$value."\.php/",
+            substr($_SERVER["REQUEST_URI"],10),
+            $matches
+            );
+    }
+?>
 <div class="col-sm-3 col-md-2 sidebar">
   <ul class="nav nav-sidebar">
-    <li class="active"><a href="index.php">Overview</a></li>
-    <li><a href="event.php">Current Event</a></li>
-    <li><a href="">Analytics</a></li>
-    <li><a href="">Export</a></li>
+    <li <?php if(curPageURL('index')) echo 'class="active"'?> ><a href="index.php">Overview</a></li>
+    <li <?php if(curPageURL('event')) echo 'class="active"'?> ><a href="event.php">Current Event</a></li>
   </ul>
   <ul class="nav nav-sidebar">
-    <li><a href="assignResource.php">Assign Resource</a></li>
-    <li><a href="createEvent.php">Create Event</a></li>
-    <li><a href="">One more nav</a></li>
-    <li><a href="">Another nav item</a></li>
-    <li><a href="">More navigation</a></li>
-  </ul>
-  <ul class="nav nav-sidebar">
-    <li><a href="">Nav item again</a></li>
-    <li><a href="">One more nav</a></li>
-    <li><a href="">Another nav item</a></li>
+    <li <?php if(curPageURL('assignResource')) echo 'class="active"'?> ><a href="assignResource.php">Assign Resource</a></li>
+    <li <?php if(curPageURL('createEvent')) echo 'class="active"'?> ><a href="createEvent.php">Create Event</a></li>
   </ul>
 </div>
